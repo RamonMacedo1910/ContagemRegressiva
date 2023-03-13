474fc092e43cc67a8bfce4257d90bc54501b6e9b
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-03-13 14:52:25 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine();
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
